8bb8afdfcaa424befc4367f22ba1e2fe2f8b237c
renshan <1005110700@qq.com>
2017-06-09 13:20:37 +0800

    提供volume、speed、pitch、rdn的配置化

--- tts.c
+++ tts.c
@@ -69,6 +69,11 @@ ZEND_METHOD(tts, __construct)
     }
 }
 
+/* }}} */
+
+/* {{{ proto setTxt(string arg)
+   设置需转换的内容 */
+
 ZEND_METHOD(tts, setTxt)
 {
     char *text;
@@ -80,8 +85,10 @@ ZEND_METHOD(tts, setTxt)
 
     zend_update_property_string(tts_ce, getThis(), "text", 4, text TSRMLS_CC);
 }
+/* }}} */
 
-
+/* {{{ protp string getTxt
+   获取转换的文字 */
 ZEND_METHOD(tts, getTxt)
 {
     zval *rv;
@@ -93,8 +100,10 @@ ZEND_METHOD(tts, getTxt)
 
 	RETURN_STR(strpprintf(0, "%s", text->value.str->val));
 }
+/* }}} */
 
-
+/* {{{ proto setDest(arg)
+   设置保存位置 */
 ZEND_METHOD(tts, setDest)
 {
     char *dest;
@@ -106,7 +115,10 @@ ZEND_METHOD(tts, setDest)
 
     zend_update_property_string(tts_ce, getThis(), "dest", 4, dest TSRMLS_CC);
 }
+/* }}} */
 
+/* {{{ protp string getDest
+   获取保存位置*/
 ZEND_METHOD(tts, getDest)
 {
     zval *rv;
@@ -118,7 +130,10 @@ ZEND_METHOD(tts, getDest)
 
     RETURN_STR(strpprintf(0, "%s", dest->value.str->val));
 }
+/* }}} */
 
+/* {{{ proto setVoice(arg)
+   设置语音类型*/
 ZEND_METHOD(tts, setVoice)
 {
     char *voice;
@@ -130,7 +145,10 @@ ZEND_METHOD(tts, setVoice)
 
     zend_update_property_string(tts_ce, getThis(), "voice", 5, voice TSRMLS_CC);
 }
+/* }}} */
 
+/* {{{ proto string getVoice
+   获取语音类型*/
 ZEND_METHOD(tts, getVoice)
 {
     zval *rv;
@@ -142,6 +160,123 @@ ZEND_METHOD(tts, getVoice)
 
     RETURN_STR(strpprintf(0, "%s", voice->value.str->val));
 }
+/* }}} */
+
+/* {{{ protp setSPeed(arg)
+   设置语速 */
+ZEND_METHOD(tts, setSpeed)
+{
+    size_t speed;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "l", &speed) == FAILURE) {
+        return;
+    }
+
+    zend_update_property_long(tts_ce, getThis(), "speed", 5, speed);
+}
+/* }}} */
+
+/* {{{ proto long getSpeed
+   获取语速 */
+ZEND_METHOD(tts, getSpeed)
+{
+    zval *rv;
+    zval *speed = zend_read_property(tts_ce, getThis(), "speed", 5, 1, rv);
+
+    if (Z_TYPE_P(speed) == IS_NULL) {
+        RETURN_NULL();
+    }
+
+    RETURN_LONG(Z_LVAL_P(speed));
+}
+/* }}} */
+
+/* {{{ proto setVolume(arg)
+   设置音量 */
+ZEND_METHOD(tts, setVolume)
+{
+    size_t volume;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "l", &volume) == FAILURE) {
+        return;
+    }
+
+    zend_update_property_long(tts_ce, getThis(), "volume", 7, volume);
+}
+/* }}} */
+
+/* {{{ proto long getVolume
+   获取音量 */
+ZEND_METHOD(tts, getVolume)
+{
+    zval *rv;
+    zval *volume = zend_read_property(tts_ce, getThis(), "volume", 7, 1, rv);
+
+    if (Z_TYPE_P(volume) == IS_NULL) {
+        RETURN_NULL();
+    }
+
+    RETURN_LONG(Z_LVAL_P(volume));
+}
+/* }}} */
+
+/* {{{ proto setPitch(arg)
+   设置音高 */
+ZEND_METHOD(tts, setPitch)
+{
+    size_t pitch;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "l", &pitch) == FAILURE) {
+        return;
+    }
+
+    zend_update_property_long(tts_ce, getThis(), "pitch", 5, pitch);
+}
+/* }}} */
+
+/* {{{ proto long getPitch
+   获取音高 */
+ZEND_METHOD(tts, getPitch)
+{
+    zval *rv;
+    zval *pitch = zend_read_property(tts_ce, getThis(), "pitch", 5, 1, rv);
+
+    if (Z_TYPE_P(pitch) == IS_NULL) {
+        RETURN_NULL();
+    }
+
+    RETURN_LONG(Z_LVAL_P(pitch));
+}
+/* }}} */
+
+/* {{{ proto setRdn(arg)
+   设置数字语音合成方式 */
+ZEND_METHOD(tts, setRdn)
+{
+    size_t rdn;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "l", &rdn) == FAILURE) {
+        return;
+    }
+
+    zend_update_property_long(tts_ce, getThis(), "rdn", 3, rdn);
+}
+/* }}} */
+
+/* {{{ proto long getRdn
+   获取语音合成方式*/
+ZEND_METHOD(tts, getRdn)
+{
+    zval *rv;
+    zval *rdn = zend_read_property(tts_ce, getThis(), "rdn", 3, 1, rv);
+
+    if (Z_TYPE_P(rdn) == IS_NULL) {
+        RETURN_NULL();
+    }
+
+    RETURN_LONG(Z_LVAL_P(rdn));
+}
+/* }}} */
 
 ZEND_METHOD(tts, run)
 {
@@ -153,15 +288,34 @@ ZEND_METHOD(tts, run)
     zval *dest;
     zval *voice;
 
+    zval *volume;
+    zval *pitch;
+    zval *speed;
+    zval *rdn;
+
     appid    = zend_read_property(tts_ce, getThis(), "appid", 5, 1, rev);
 
     text  = zend_read_property(tts_ce, getThis(), "text", 4, 1, rev);
     dest  = zend_read_property(tts_ce, getThis(), "dest", 4, 1, rev);
     voice = zend_read_property(tts_ce, getThis(), "voice", 5, 1, rev);
 
+    volume = zend_read_property(tts_ce, getThis(), "volume", 7, 1, rev);
+    pitch  = zend_read_property(tts_ce, getThis(), "pitch", 5, 1, rev);
+    speed  = zend_read_property(tts_ce, getThis(), "speed", 5, 1, rev);
+    rdn    = zend_read_property(tts_ce, getThis(), "rdn", 3, 1, rev);
+
+    convert_to_string(volume);
+    convert_to_string(pitch);
+    convert_to_string(speed);
+    convert_to_string(rdn);
+
     if (Z_TYPE_P(appid)    == IS_NULL ||
         Z_TYPE_P(text)     == IS_NULL ||
         Z_TYPE_P(dest)     == IS_NULL ||
+        Z_TYPE_P(volume)   == IS_NULL ||
+        Z_TYPE_P(pitch)    == IS_NULL ||
+        Z_TYPE_P(speed)    == IS_NULL ||
+        Z_TYPE_P(rdn)      == IS_NULL ||
         Z_TYPE_P(voice)    == IS_NULL) {
         RETURN_LONG(-1);
     }
@@ -182,10 +336,11 @@ ZEND_METHOD(tts, run)
      *
      * 详细参数说明请参阅《讯飞语音云MSC--API文档》
      */
-    /* const char* session_begin_params = "voice_name = xiaoyan, text_encoding = utf8, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2"; */
-    char *session_begin_params = safe_emalloc(1, Z_STRLEN_P(voice) + 102, 1);
-    sprintf(session_begin_params, "voice_name = %s, text_encoding = utf8, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2", voice->value.str->val);
 
+    /* const char* session_begin_params = "voice_name = xiaoyan, text_encoding = utf8, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2"; */
+    char *session_begin_params = safe_emalloc(1, Z_STRLEN_P(voice) + Z_STRLEN_P(speed) + Z_STRLEN_P(volume) + Z_STRLEN_P(pitch) + Z_STRLEN_P(rdn) + 102, 1);
+    sprintf(session_begin_params, "voice_name = %s, text_encoding = utf8, sample_rate = 16000, speed = %s, volume = %s, pitch = %s, rdn = %s",
+            voice->value.str->val, Z_STRVAL_P(speed), Z_STRVAL_P(volume), Z_STRVAL_P(pitch), Z_STRVAL_P(rdn));
 
     char *_text = safe_emalloc(1, Z_STRLEN_P(text), 1);
     sprintf(_text, "%s", Z_STRVAL_P(text));//登录参数,appid与msc库绑定,请勿随意改动
@@ -253,6 +408,14 @@ const zend_function_entry tts_functions[] = {
     ZEND_ME(tts, getDest, NULL, ZEND_ACC_PUBLIC)
     ZEND_ME(tts, setVoice, NULL, ZEND_ACC_PUBLIC)
     ZEND_ME(tts, getVoice, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, setSpeed, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, getSpeed, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, setVolume, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, getVolume, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, setPitch, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, getPitch, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, setRdn, NULL, ZEND_ACC_PUBLIC)
+    ZEND_ME(tts, getRdn, NULL, ZEND_ACC_PUBLIC)
     ZEND_ME(tts, run, NULL, ZEND_ACC_PUBLIC)
 
     ZEND_FE_END
@@ -281,6 +444,39 @@ PHP_MINIT_FUNCTION(tts)
 
     /* 默认声音是xiaoyan */
 	zend_declare_property_string(tts_ce, "voice", 5, "xiaoyan", ZEND_ACC_PRIVATE TSRMLS_DC);
+
+    /* 默认语速是50 */
+	zend_declare_property_long(tts_ce, "speed", 5, 50, ZEND_ACC_PRIVATE TSRMLS_DC);
+
+    /* 默认音量是50 */
+	zend_declare_property_long(tts_ce, "volume", 7, 50, ZEND_ACC_PRIVATE TSRMLS_DC);
+
+    /* 默认音高是50 */
+	zend_declare_property_long(tts_ce, "pitch", 5, 50, ZEND_ACC_PRIVATE TSRMLS_DC);
+
+    /* 默认语音合成方式 */
+	zend_declare_property_long(tts_ce, "rdn", 3, 2, ZEND_ACC_PRIVATE TSRMLS_DC);
+
+    ///
+    ///  语音合成设置的常量
+    ///  用法：
+    ///  $tts->setPitch(TTS::TTS_RN_AUTO_VALUE); // $tts->setPitch(0);
+    ///  $tts->setPitch(TTS::TTS_RN_RN_VALUE);   // $tts->setPitch(1);
+    ///  $tts->setPitch(TTS::TTS_RN_RN_DIGIT);   // $tts->setPitch(2);
+    ///  $tts->setPitch(TTS::TTS_RN_AUTO_DIGIT); // $tts->setPitch(3);
+    ///
+
+    /* 自动，不确定时按照值发音*/
+    zend_declare_class_constant_long(tts_ce, "TTS_RN_AUTO_VALUE ", 17, 0 TSRMLS_DC);
+
+    /* 按照值发音 */
+    zend_declare_class_constant_long(tts_ce, "TTS_RN_VALUE ", 12, 1 TSRMLS_DC);
+
+    /* 按照 串发音*/
+    zend_declare_class_constant_long(tts_ce, "TTS_RN_DIGIT ", 12, 2 TSRMLS_DC);
+
+    /*自动，不确定时按照串发音 */
+    zend_declare_class_constant_long(tts_ce, "TTS_RN_AUTO_DIGIT ", 17, 3 TSRMLS_DC);
 
 	return SUCCESS;
 }
@@ -327,7 +523,7 @@ PHP_MINFO_FUNCTION(tts)
     php_info_print_table_row(2, "Default voice name", "xiaoyan");
     php_info_print_table_row(2, "Text encoding", "utf8");
     php_info_print_table_row(2, "Sample rate", "16000");
-    php_info_print_table_row(2, "Speed", "50");
+    php_info_print_table_row(2, "Default Speed", "50");
     php_info_print_table_row(2, "Volume", "50");
     php_info_print_table_row(2, "Pitch", "50");
     php_info_print_table_row(2, "Rdn", "2");
