ba1f328ff896a4bd3dbb102871da0322f7055fdd
renshan <1005110700@qq.com>
2018-01-11 11:26:32 +0800

    现在支持红帽系列和debian系列的32和64位系统

--- auto_conf.sh
+++ auto_conf.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-sudo cp ./tts_libs/x64/libmsc.so.bk ./tts_libs/x64/libmsc.so
-sudo cp ./tts_libs/x64/libmsc.so /lib/x86_64-linux-gnu/
+
+### 检测是redhat系列还是debian系列
+apt=`which apt 2>&1 | awk '{print $1}' | grep apt`
+yum=`which yum 2>&1 | awk '{print $1}' | grep yum`
+
+### 检测是32位还是64位
+arch=`arch`
+
+if [ "$arch" == "x86_64" ];then
+    lib=./tts_libs/x64/libmsc.so.bk
+else
+    lib=./tts_libs/x86/libmsc.so.bk
+fi
+
+### redhat 系列
+if [ -x "$yum" ];then
+    if [ "$arch" == "x86_64" ];then
+        cp $lib /usr/lib64/libmsc.so
+    else
+        cp $lib /usr/lib/libmsc.so
+    fi
+fi
+
+### debian 系列
+if [ -x "$apt" ];then
+    if [ "$arch" == "x86_64" ];then
+        cp $lib /lib/x86_64-linux-gnu/libmsc.so
+    else
+        cp $lib /lib/libmsc.so
+    fi
+fi
+
 sudo ldconfig
 sudo phpize && ./configure LDFLAGS="-L./tts_libs/x64 -lmsc -lrt -ldl -lpthread"
-sudo rm ./tts_libs/x64/libmsc.so
